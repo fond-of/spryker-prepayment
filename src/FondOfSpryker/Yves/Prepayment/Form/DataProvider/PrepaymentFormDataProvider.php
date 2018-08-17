4cd3ce7c23b7da64eaf304e2ac281eb776dea533
Daniel Rose <daniel-rose@gmx.de>
2018-08-17 12:35:45 +0200

    add paymentransfer if not exists

--- src/FondOfSpryker/Yves/Prepayment/Form/DataProvider/PrepaymentFormDataProvider.php
+++ src/FondOfSpryker/Yves/Prepayment/Form/DataProvider/PrepaymentFormDataProvider.php
@@ -2,6 +2,7 @@
 
 namespace FondOfSpryker\Yves\Prepayment\Form\DataProvider;
 
+use Generated\Shared\Transfer\PaymentTransfer;
 use Spryker\Shared\Kernel\Transfer\AbstractTransfer;
 use Spryker\Yves\StepEngine\Dependency\Form\StepEngineFormDataProviderInterface;
 
@@ -14,6 +15,12 @@ class PrepaymentFormDataProvider implements StepEngineFormDataProviderInterface
      */
     public function getData(AbstractTransfer $dataTransfer)
     {
+        if ($dataTransfer->getPayment() === null) {
+            $paymentTransfer = new PaymentTransfer();
+            
+            $dataTransfer->setPayment($paymentTransfer);
+        }
+        
         return $dataTransfer;
     }
 
